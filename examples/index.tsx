26a76021b9e366caf39d1f9247380630a94e6d60
Amirul Ashraf <asdacap@gmail.com>
2022-04-28 00:47:44 +0800

    Boilerplate codes

---
+++ examples/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import { MyComponent } from '..';
+
+ReactDOM.render(
+    <MyComponent />,
+    document.getElementById('root')
+);
+
---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+
+export function MyComponent() {
+    return <h1>Test</h1>;
+}
